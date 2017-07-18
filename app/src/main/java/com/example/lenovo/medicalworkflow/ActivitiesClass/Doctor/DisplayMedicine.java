8b468875eae43738111bd0d7fea06482e0b53f9f
Patiko <patluk1@o2.pl>
2017-07-19 00:17:30 +0200

    New libraries, files handler

--- app/src/main/java/com/example/lenovo/medicalworkflow/ActivitiesClass/Doctor/DisplayMedicine.java
+++ app/src/main/java/com/example/lenovo/medicalworkflow/ActivitiesClass/Doctor/DisplayMedicine.java
@@ -4,12 +4,18 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.AssetManager;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.annotation.IdRes;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -22,11 +28,24 @@ import com.example.lenovo.medicalworkflow.Database.DBHelper;
 import com.example.lenovo.medicalworkflow.ActivitiesClass.LoginActivity;
 import com.example.lenovo.medicalworkflow.R;
 
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import jxl.Cell;
+import jxl.Sheet;
+import jxl.Workbook;
+
 /**
  * Created by Lenovo on 07.05.2017.
  */
 
-public class DisplayMedicine extends Activity {
+public class DisplayMedicine extends AppCompatActivity {
     int from_Where_I_Am_Coming = 0;
     private DBHelper mydb ;
     ImageView deleteIcon;
@@ -40,6 +59,8 @@ public class DisplayMedicine extends Activity {
     RadioButton radio1Refund;
     RadioButton radio2Refund;
     SharedPreferences sharedPreferences;
+    Button checkRefundBtn;
+    ListView excelRecords;
 
 
 
@@ -54,6 +75,8 @@ public class DisplayMedicine extends Activity {
         type = (TextView) findViewById(R.id.editTextType);
         quantity = (TextView) findViewById(R.id.editTextQuantity);
         //refundable = (TextView) findViewById(R.id.textView4);
+        checkRefundBtn = (Button) findViewById(R.id.checkRefundBtn);
+       // excelRecords = (ListView) findViewById(R.id.excelRecords);
 
 
         injection_way = (TextView) findViewById(R.id.editTextInjectionWay);
@@ -158,8 +181,125 @@ public class DisplayMedicine extends Activity {
             editB.setVisibility(View.GONE);
             deleteB.setVisibility(View.GONE);
         }
+    /*    StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        try {
+            saveUrl("zalacznik-do-obwieszczenia-1.xls","http://www.mz.gov.pl/wp-content/uploads/2017/04/zalacznik-do-obwieszczenia-1.xls");
+            Toast.makeText(getApplicationContext(), "Plik pobrany",
+                    Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+
+   /*     new Thread(new Runnable(){
+            @Override
+            public void run() {
+                // Do network action in this function
+
+            }
+        }).start();*/
 
 
+
+
+    }
+
+    public void checkRefund(View view){
+        try {
+
+            AssetManager am =getAssets();
+            //InputStream is=am.open("zalacznik-do-obwieszczenia-nowy.xls");
+            InputStream is=am.open("zalacznik-do-obwieszczenia-1.xls");
+
+            Workbook wb = Workbook.getWorkbook(is);
+            Sheet s=wb.getSheet(0);
+            int row = s.getRows();
+            int col = s.getColumns();
+            String xx="";
+            String subString="";
+            List<String> arrayList= new ArrayList<String>();
+/*
+                 for (int i=3; i<row; i++){
+                //for(int c=0; c<col; c++){
+                    Cell z = s.getCell(2,i);
+                    xx =xx+z.getContents();
+                     xx=xx+"\n";
+                }
+              //  }
+            display(xx);*/
+            for (int i=3; i<row; i++){
+                //for(int c=0; c<col; c++){
+                    Cell z = s.getCell(2,i);
+                    xx="";
+                    xx =xx+z.getContents();
+                int iend=xx.indexOf(",");
+                if(iend!= -1){
+                    subString="";
+                    subString = xx.substring(0,iend);
+                    arrayList.add(subString);
+                }
+                //subString=subString+"\n";
+
+              //  }
+
+            }
+
+            //display(subString);
+         //   String finalList;
+         //   displayList(arrayList);
+            displayTextView(arrayList);
+        }
+        catch (Exception e){
+        }
+    }
+        public void displayList(List<String> arrayList){
+            ArrayAdapter arrayAdapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1, arrayList);
+
+           ListView excelRecords = (ListView)findViewById(R.id.excelRecords);
+            excelRecords.setAdapter(arrayAdapter);
+            arrayAdapter.notifyDataSetChanged();
+
+    }
+
+    public void displayTextView(List<String> arrayList){
+        String finalStr="";
+        for(int j=0; j<arrayList.size();j++){
+            finalStr=finalStr+arrayList.get(j);
+            finalStr=finalStr+"\n";
+
+        }
+        TextView x = (TextView)findViewById(R.id.textView0);
+        x.setText(finalStr);
+    }
+/*    public void display(String value){
+        TextView x = (TextView)findViewById(R.id.textView0);
+        x.setText(value);
+    }*/
+
+
+
+    public void saveUrl(final String filename, final String urlString)
+            throws MalformedURLException, IOException {
+        BufferedInputStream in = null;
+        FileOutputStream fout = null;
+
+        try {
+            in = new BufferedInputStream(new URL(urlString).openStream());
+            fout = new FileOutputStream(filename);
+
+            final byte data[] = new byte[1024];
+            int count;
+            while ((count = in.read(data, 0, 1024)) != -1) {
+                fout.write(data, 0, count);
+            }
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+            if (fout != null) {
+                fout.close();
+            }
+        }
     }
 
 
@@ -234,48 +374,6 @@ public class DisplayMedicine extends Activity {
 
 
 
-/*    public void insertMedicine(View view) {
-
-
-        if (!name.getText().toString().equals("") && !type.getText().toString().equals("") && !quantity.getText().toString().equals("")
-            //       && !refundable.getText().toString().equals("")
-                ) {
-            //    sharedPreferences = getSharedPreferences(LoginActivity.MyPREFERENCES, Context.MODE_PRIVATE);
-            Integer creator_id = sharedPreferences.getInt(LoginActivity.UserId,0);
-            Integer submission_id = mydb.insertMedicine(name.getText().toString(), type.getText().toString(),
-                    quantity.getText().toString(), refundable.getText().toString(),
-                    injection_way.getText().toString(),creator_id);
-            if (submission_id!=0) {
-
-                Toast.makeText(getApplicationContext(), "Lek został dodany",
-                        Toast.LENGTH_SHORT).show();
-
-            } else {
-                Toast.makeText(getApplicationContext(), "Lek niedodany",
-                        Toast.LENGTH_SHORT).show();
-            }
-
-            Intent intent = new Intent(getApplicationContext(), CheckMedicineList.class);
-            startActivity(intent);
-
-
-        } else if (name.getText().toString().equals("")){
-            Toast.makeText(getApplicationContext(), "Proszę podać nazwę leku",
-                    Toast.LENGTH_LONG).show();
-        } else if(type.getText().toString().equals("")){
-            Toast.makeText(getApplicationContext(), "Proszę podać typ",
-                    Toast.LENGTH_LONG).show();
-        } else if(quantity.getText().toString().equals("")){
-            Toast.makeText(getApplicationContext(), "Proszę podać ilość",
-                    Toast.LENGTH_LONG).show();
-        } else if(refundable.getText().toString().equals("")){
-            Toast.makeText(getApplicationContext(), "Proszę wybrać czy lek jest refundowalny",
-                    Toast.LENGTH_LONG).show();
-        }
-    }*/
-
-
-
     public void insertMedicine(View view) {
 
 
@@ -313,6 +411,14 @@ public class DisplayMedicine extends Activity {
     }
 
 
+
+    public void refundYesClicked(View v) {
+        checkRefundBtn.setVisibility(View.VISIBLE);
+
+    }
+    public void refundNoClicked(View v) {
+        checkRefundBtn.setVisibility(View.GONE);
+    }
 
 
 
