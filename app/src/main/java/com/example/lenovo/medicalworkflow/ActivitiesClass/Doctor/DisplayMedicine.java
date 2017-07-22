3cb34dce1077a77259de654b60418afd8a393324
Patiko <patluk1@o2.pl>
2017-07-22 19:14:47 +0200

    Whole autocomplete functionality regarding add medicine implemented.

--- app/src/main/java/com/example/lenovo/medicalworkflow/ActivitiesClass/Doctor/DisplayMedicine.java
+++ app/src/main/java/com/example/lenovo/medicalworkflow/ActivitiesClass/Doctor/DisplayMedicine.java
@@ -8,6 +8,7 @@ import android.database.Cursor;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
@@ -27,9 +28,12 @@ import com.example.lenovo.medicalworkflow.ActivitiesClass.LoginActivity;
 import com.example.lenovo.medicalworkflow.R;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -65,6 +69,10 @@ public class DisplayMedicine extends AppCompatActivity {
     String typeListType="type";
     String dosageListType="dosage";
     String quantityListType="quantity";
+    ArrayAdapter adapterName;
+    ArrayAdapter adapterType;
+    ArrayAdapter adapterDosage;
+    ArrayAdapter adapterQuantity;
 
     TextView injection_way;
     int id_To_Update = 0;
@@ -105,13 +113,14 @@ public class DisplayMedicine extends AppCompatActivity {
         });
 
 
-
         mydb = new DBHelper(this);
         sharedPreferences = getSharedPreferences(LoginActivity.MyPREFERENCES,Context.MODE_PRIVATE);
         int Value = sharedPreferences.getInt(LoginActivity.UsedMedicineId,0);
         Button b = (Button)findViewById(R.id.button1);
         Button editB = (Button)findViewById(R.id.button2);
         Button deleteB = (Button)findViewById(R.id.button3);
+        checkRefundBtn.setVisibility(View.GONE);
+
 
         if(Value>0){
                 //means this is the view part not the add contact part.
@@ -202,31 +211,110 @@ public class DisplayMedicine extends AppCompatActivity {
 
             }
         }).start();*/
-        ArrayAdapter adapterName = new ArrayAdapter(this,android.R.layout.simple_list_item_1,getDependingParameterList(nameListType));
-        nameAC.setAdapter(adapterName);
-        nameAC.setThreshold(3);
 
-        ArrayAdapter adapterType = new ArrayAdapter(this,android.R.layout.simple_list_item_1,getDependingParameterList(typeListType));
-        typeAC.setAdapter(adapterType);
-        typeAC.setThreshold(2);
 
-        ArrayAdapter adapterDosage = new ArrayAdapter(this,android.R.layout.simple_list_item_1,getDependingParameterList(dosageListType));
+
+      /*  typeAC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                adapterType = new ArrayAdapter(DisplayMedicine.this,android.R.layout.simple_list_item_1,getValuesForAdapterLists(typeListType));
+                typeAC.setAdapter(adapterType);
+                typeAC.setThreshold(2);
+               *//* adapterDosage = new ArrayAdapter(this,android.R.layout.simple_list_item_1,getValuesForAdapterLists(typeListType));
+                dosageAC.setAdapter(adapterDosage);
+                adapterQuantity = new ArrayAdapter(this,android.R.layout.simple_list_item_1,getValuesForAdapterLists(typeListType));
+                quantityAC.setAdapter(adapterQuantity);*//*
+                // customerAutocomplete.dismissDropDown();
+
+            }
+        });*/
+
+
+
+      //  adapterType = new ArrayAdapter(this,android.R.layout.simple_list_item_1,getDependingParameterList(typeListType));
+      //  adapterType = new ArrayAdapter(this,android.R.layout.simple_list_item_1,getValuesForAdapterLists(typeListType));
+     //   typeAC.setAdapter(adapterType);
+     //   typeAC.setThreshold(2);
+
+        /*adapterDosage = new ArrayAdapter(this,android.R.layout.simple_list_item_1,getValuesForAdapterLists(typeListType));
         dosageAC.setAdapter(adapterDosage);
         dosageAC.setThreshold(1);
-
-        ArrayAdapter adapterQuantity = new ArrayAdapter(this,android.R.layout.simple_list_item_1,getDependingParameterList(quantityListType));
+*/
+       /* adapterQuantity = new ArrayAdapter(this,android.R.layout.simple_list_item_1,getValuesForAdapterLists(typeListType));
         quantityAC.setAdapter(adapterQuantity);
         quantityAC.setThreshold(1);
+*/
 
 
     }
+    public void loadAutoCompleteList(View view){
+
+    }
+    public List<String> getValuesForAdapterLists(String list_type){
+        List<String> adapterList = new ArrayList<String>();
+
+        List<String> strList= new ArrayList<String>();
+        List<Integer> indexList = new ArrayList<Integer>();
+        String nameACStr = nameAC.getText().toString();
+        indexList = getIdsOfProvidedName(nameACStr);
+
+
+        if(list_type.equals(typeListType)){
+            strList = getDependingParameterList(typeListType);
+            for(int i=0; i< indexList.size(); i++){
+                for(int j=0; j<strList.size(); j++){
+                    if(indexList.get(i).equals(j)){
+                        adapterList.add(strList.get(j));
+                    }
+                }
+
+            }
+        } else if(list_type.equals(dosageListType)){
+            strList = getDependingParameterList(dosageListType);
+            for(int i=0; i< indexList.size(); i++){
+                for(int j=0; j<strList.size(); j++){
+                    if(indexList.get(i).equals(j)){
+                        adapterList.add(strList.get(j));
+                    }
+                }
+
+            }
+        } else if(list_type.equals(quantityListType)){
+            strList = getDependingParameterList(quantityListType);
+            for(int i=0; i< indexList.size(); i++){
+                for(int j=0; j<strList.size(); j++){
+                    if(indexList.get(i).equals(j)){
+                        adapterList.add(strList.get(j));
+                    }
+                }
+
+            }
+        }
+
+        return adapterList;
+    }
+    public List<Integer> getIdsOfProvidedName(String name) {
+        List<Integer> indexList = new ArrayList<Integer>();
+
+        List<String> stringList = new ArrayList<String>();
+
+        stringList = getDependingParameterList(nameListType);
+        for (int i = 0; i < stringList.size(); i++) {
+            if(stringList.get(i).contains(name)){
+                indexList.add(i);
+
+            }
+        }
+        return indexList;
+    }
+
     public List<String> getDependingParameterList(String list_type){
         List<String> lista= new ArrayList<String>();
         try {
-
             AssetManager am =getAssets();
             //InputStream is=am.open("zalacznik-do-obwieszczenia-nowy.xls");
             InputStream is=am.open("zalacznik-do-obwieszczenia-1.xls");
+
             String xxNames="";
             String subStringNames="";
             String xxDosages="";
@@ -267,10 +355,18 @@ public class DisplayMedicine extends AppCompatActivity {
                     xxTypes="";
                     xxTypes =xxTypes+zTypes.getContents();
                     subStringTypes="";
-                    int lastIndexType = xxTypes.lastIndexOf(",");
+                    /*int lastIndexType = xxTypes.lastIndexOf(",");
                     int prevIndexType = xxTypes.lastIndexOf(",", lastIndexType - 1);
                     if(prevIndexType!=-1 && lastIndexType!=-1){
-                        subStringTypes = xxTypes.substring(prevIndexType +1, lastIndexType).trim();
+                        subStringTypes = i + " "+xxTypes.substring(prevIndexType +1, lastIndexType).trim();
+                       // StringBuilder sb = new StringBuilder();
+                        medicineTypeListExcel.add(subStringTypes);
+                    }*/
+
+                    int firstIndexType = xxTypes.indexOf(",");
+                    int secondIndexType = xxTypes.indexOf(",", firstIndexType + 1);
+                    if(secondIndexType!=-1 && firstIndexType!=-1){
+                        subStringTypes = i + " "+xxTypes.substring(firstIndexType+1, secondIndexType).trim();
                         medicineTypeListExcel.add(subStringTypes);
                     }
                 }
@@ -284,7 +380,7 @@ public class DisplayMedicine extends AppCompatActivity {
 
                     if(iendDosages!= -1){
                         subStringDosages="";
-                        subStringDosages = xxDosages.substring(xxDosages.lastIndexOf(',')+2).trim();
+                        subStringDosages = i + " "+ xxDosages.substring(xxDosages.lastIndexOf(',')+2).trim();
                         medicineDosageListExcel.add(subStringDosages);
                     }
                 }
@@ -360,26 +456,7 @@ public class DisplayMedicine extends AppCompatActivity {
                     medicineNameListExcel.add(subStringNames);
                 }
             }
-            if(searchIfExists(name.getText().toString(),medicineNameListExcel)){
-                Toast.makeText(getApplicationContext(), "Lek jest refundowany!",
-                        Toast.LENGTH_SHORT).show();
-                //  radio1Refund.setClickable(false);
-                // radio2Refund.setClickable(false);
-            }else {
-                Toast.makeText(getApplicationContext(), "Lek nie jest refundowany!",
-                        Toast.LENGTH_SHORT).show();
 
-            }
-            indexNameList = getIndexListEnteredNames(name.getText().toString(),medicineNameListExcel);  //ZWROC LISTE INDEXow GDZIE WYSTEPUJE PODANY NAME
-            String textToDisplay="";
-            if(!indexNameList.isEmpty()){
-                for(int h=0; h<indexNameList.size(); h++){
-                    textToDisplay=textToDisplay+Integer.toString(indexNameList.get(h));
-                    textToDisplay=textToDisplay+"\n";
-                }
-                TextView xxx=(TextView) findViewById(R.id.textView0);
-                xxx.setText(textToDisplay);
-            }
          //   displayTextView(medicineNameListExcel);
 
           /////////////// MEDICINE TYPE LIST /////////////////
@@ -393,25 +470,14 @@ public class DisplayMedicine extends AppCompatActivity {
                 //if(iendDosages!= -1){
                 subStringTypes="";
 
-                int lastIndexType = xxTypes.lastIndexOf(",");
-                int prevIndexType = xxTypes.lastIndexOf(",", lastIndexType - 1);
-
-                if(prevIndexType!=-1 && lastIndexType!=-1){
-                    subStringTypes = xxTypes.substring(prevIndexType +1, lastIndexType).trim();
-                    //subStringTypes = xxTypes.substring(typeStart, typeEnd);
+                int firstIndexType = xxTypes.indexOf(",");
+                int secondIndexType = xxTypes.indexOf(",", firstIndexType + 1);
+                if(secondIndexType!=-1 && firstIndexType!=-1){
+                    subStringTypes = i + " "+xxTypes.substring(firstIndexType+1, secondIndexType).trim();
                     medicineTypeListExcel.add(subStringTypes);
                 }
             }
-            if(searchIfExists(type.getText().toString(),medicineTypeListExcel)){
-                Toast.makeText(getApplicationContext(), "Lek jest refundowany! POSTAC",
-                        Toast.LENGTH_SHORT).show();
-                //  radio1Refund.setClickable(false);
-                // radio2Refund.setClickable(false);
-            }else {
-                Toast.makeText(getApplicationContext(), "Lek nie jest refundowany! POSTAC",
-                        Toast.LENGTH_SHORT).show();
 
-            }
               // displayTextView(medicineTypeListExcel);
 
             ///////////// MEDICINE DOSAGE LIST /////////////////
@@ -425,20 +491,13 @@ public class DisplayMedicine extends AppCompatActivity {
 
                 if(iendDosages!= -1){
                     subStringDosages="";
-                    subStringDosages = xxDosages.substring(xxDosages.lastIndexOf(',')+2).trim();
+                    subStringDosages = xxDosages.substring(xxDosages.lastIndexOf(',')+2);
                     medicineDosageListExcel.add(subStringDosages);
+                    /*if(subStringDosages.equals(""))
+                        Toast.makeText(getApplicationContext(), "Lek jest refundowany! DAWKA" +i+1, Toast.LENGTH_SHORT).show();*/
                 }
             }
-            if(searchIfExists(dosage.getText().toString(),medicineDosageListExcel)){
-                Toast.makeText(getApplicationContext(), "Lek jest refundowany! DAWKA",
-                        Toast.LENGTH_SHORT).show();
-                //  radio1Refund.setClickable(false);
-                // radio2Refund.setClickable(false);
-            }else {
-                Toast.makeText(getApplicationContext(), "Lek nie jest refundowany! DAWKA",
-                        Toast.LENGTH_SHORT).show();
 
-            }
          //   displayTextView(medicineDosageListExcel);
 
             ///////////// MEDICINE QUANTITY LIST /////////////////
@@ -452,22 +511,105 @@ public class DisplayMedicine extends AppCompatActivity {
                 stringQuantity = xxQuantity.trim();
                 medicineQuantityListExcel.add(stringQuantity);
             }
-            if(searchIfExists(quantity.getText().toString(),medicineQuantityListExcel)){
-                Toast.makeText(getApplicationContext(), "Lek jest refundowany! ZAWARTOSC OPAKOWANIA",
-                        Toast.LENGTH_SHORT).show();
-                //  radio1Refund.setClickable(false);
-                // radio2Refund.setClickable(false);
-            }else {
-                Toast.makeText(getApplicationContext(), "Lek nie jest refundowany! ZAWARTOSC OPAKOWANIA",
-                        Toast.LENGTH_SHORT).show();
-
+            // displayTextView(medicineQuantityListExcel);
+            if(searchIfExists(typeAC.getText().toString(),medicineTypeListExcel) && searchIfExists(nameAC.getText().toString(),medicineNameListExcel) &&
+                    searchIfExists(dosageAC.getText().toString(),medicineDosageListExcel) && searchIfExists(quantityAC.getText().toString(),medicineQuantityListExcel) ){
+                Toast.makeText(getApplicationContext(), "Lek jest refundowany!", Toast.LENGTH_LONG).show();
+            } else if(!searchIfExists(nameAC.getText().toString(),medicineNameListExcel)){
+                Toast.makeText(getApplicationContext(), "Lek nie jest refundowany!", Toast.LENGTH_LONG).show();
+            } else if(!searchIfExists(typeAC.getText().toString(),medicineTypeListExcel)){
+                Toast.makeText(getApplicationContext(), "Lek nie jest refundowany! Postać nie pasuje do leku refundowanego!", Toast.LENGTH_LONG).show();
+            } else if(!searchIfExists(dosageAC.getText().toString(),medicineDosageListExcel)){
+                Toast.makeText(getApplicationContext(), "Lek nie jest refundowany! Dawka nie pasuje do leku refundowanego!", Toast.LENGTH_LONG).show();
+            } else if(!searchIfExists(quantityAC.getText().toString(),medicineQuantityListExcel)){
+                Toast.makeText(getApplicationContext(), "Lek nie jest refundowany! Zawartość opakowania nie pasuje do leku refundowanego!", Toast.LENGTH_LONG).show();
             }
-            displayTextView(medicineQuantityListExcel);
 
         }
         catch (Exception e){
         }
     }
+
+/*    @Override
+    public void onResume() {
+        super.onResume();
+
+    }*/
+
+        public void refundYesClicked(View v) {
+            checkRefundBtn.setVisibility(View.VISIBLE);
+            adapterName = new ArrayAdapter(DisplayMedicine.this,android.R.layout.simple_list_item_1,getDependingParameterList(nameListType));
+            nameAC.setAdapter(adapterName);
+            nameAC.setThreshold(2);
+
+            typeAC.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    if (event.getAction() == MotionEvent.ACTION_UP && !v.hasFocus()) {
+                        // onClick() is not called when the EditText doesn't have focus,
+                        // onFocusChange() is called instead, which might have a different
+                        // meaning. This condition calls onClick() when click was performed
+                        // but wasn't reported. Condition can be extended for v.isClickable()
+                        // or v.isEnabled() if needed. Returning false means that everything
+                        // else behaves as before.
+                        adapterType = new ArrayAdapter(DisplayMedicine.this,android.R.layout.simple_list_item_1,getValuesForAdapterLists(typeListType));
+                        typeAC.setAdapter(adapterType);
+                        typeAC.setThreshold(1);
+                        v.performClick();
+                    }
+                    return false;
+                }
+            });
+
+            dosageAC.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    if (event.getAction() == MotionEvent.ACTION_UP && !v.hasFocus()) {
+                        // onClick() is not called when the EditText doesn't have focus,
+                        // onFocusChange() is called instead, which might have a different
+                        // meaning. This condition calls onClick() when click was performed
+                        // but wasn't reported. Condition can be extended for v.isClickable()
+                        // or v.isEnabled() if needed. Returning false means that everything
+                        // else behaves as before.
+                        adapterDosage = new ArrayAdapter(DisplayMedicine.this,android.R.layout.simple_list_item_1,getValuesForAdapterLists(dosageListType));
+                        dosageAC.setAdapter(adapterDosage);
+                        dosageAC.setThreshold(1);
+                        v.performClick();
+                    }
+                    return false;
+                }
+            });
+
+            quantityAC.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    if (event.getAction() == MotionEvent.ACTION_UP && !v.hasFocus()) {
+                        // onClick() is not called when the EditText doesn't have focus,
+                        // onFocusChange() is called instead, which might have a different
+                        // meaning. This condition calls onClick() when click was performed
+                        // but wasn't reported. Condition can be extended for v.isClickable()
+                        // or v.isEnabled() if needed. Returning false means that everything
+                        // else behaves as before.
+                        adapterQuantity = new ArrayAdapter(DisplayMedicine.this,android.R.layout.simple_list_item_1,getValuesForAdapterLists(quantityListType));
+                        quantityAC.setAdapter(adapterQuantity);
+                        quantityAC.setThreshold(1);
+                        v.performClick();
+                    }
+                    return false;
+                }
+            });
+
+    }
+
+    public void refundNoClicked(View v) {
+        checkRefundBtn.setVisibility(View.GONE);
+        nameAC.setAdapter(null);
+        typeAC.setAdapter(null);
+        dosageAC.setAdapter(null);
+        quantityAC.setAdapter(null);
+
+    }
+
     public List<Integer> getIndexListEnteredNames(String name, List<String> lista){
 
         List<Integer> indexList= new ArrayList<Integer>();
@@ -606,18 +748,6 @@ public class DisplayMedicine extends AppCompatActivity {
                         Toast.LENGTH_LONG).show();
             }
     }
-
-
-
-    public void refundYesClicked(View v) {
-        checkRefundBtn.setVisibility(View.VISIBLE);
-    }
-
-    public void refundNoClicked(View v) {
-        checkRefundBtn.setVisibility(View.GONE);
-    }
-
-
 
     public void updateMedicine(View view) {
 
